714c95c5a8385af24c247a593ec3ba5374f5c394
Yunzhu Jin <jyz022020@gmail.com>
2022-03-26 23:26:08 -0700

    rr

---
+++ app.js
@@ -0,0 +1,23 @@
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const profileDataArgs = process.argv.slice(2);
+
+// console.log(profileDataArgs);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+const message = 'Hello Node!';
+message = 'Goodbye Node!';
+
+const sum = 5 + 3;
+sum += 1;
